c645ad8105cd1c611a4c15b3f6a692f166c0b006
nima-salehi <nima-salehi@hotmail.com>
2016-01-15 19:33:16 +0330

    Building the Data Model - Rolling Back & Resetting Migrations, Creating Database Seeders

--- blog/app/Description.php
+++ blog/app/Description.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Description extends Model
 {
+    //Building the Data Model - Rolling Back & Resetting Migrations, Creating Database Seeders
+    return $this->belongsTo(Product::class);
     //
 }
--- blog/app/Product.php
+++ blog/app/Product.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+   //Building the Data Model - Rolling Back & Resetting Migrations, Creating Database Seeders
+    return $this->hasMany(Description::class);
+   //
 }
